0271fec5c9bb085d99b8a066b7b38abbcbb5db1f
TAKANOME-DEV <79809121+TAKANOME-DEV@users.noreply.github.com>
2021-10-12 15:56:37 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const body = document.getElementById("body");
+const content = document.querySelector(".content");
+const input = document.getElementById("input");
+
+const users = [];
+
+input.addEventListener("input", (e) => filterUser(e.target.value));
+
+const filterUser = (search) => {
+  users.map((user) => {
+    if (user.innerText.toLowerCase().includes(search.toLowerCase())) {
+      return user.classList.remove("hide");
+    } else {
+      user.classList.add("hide");
+    }
+  });
+};
+
+const displayUser = async () => {
+  const res = await fetch("https://randomuser.me/api?results=100");
+  const { results } = await res.json();
+
+  results.map((user) => {
+    const card = document.createElement("div");
+    const cardHeader = document.createElement("div");
+    const cardContent = document.createElement("div");
+
+    card.classList.add("card");
+    cardHeader.classList.add("card-header");
+    cardContent.classList.add("card-content");
+
+    users.push(card);
+
+    cardHeader.innerHTML = `
+      <img src="https://images.unsplash.com/photo-1603302576837-37561b2e2302?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bGFwdG9wc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
+      alt=""/>
+      `;
+
+    cardContent.innerHTML = `
+      <h3>
+      ${user.location.city}, ${user.location.country}
+      </h3>
+      <p>
+        ${user.email}
+      </p>
+      <div class="user">
+        <div class="user-img">
+          <img
+            src="${user.picture.large}"
+            alt="${user.name.first}"
+          />
+        </div>
+        <div class="user-info">
+          <h4>${user.name.first} ${user.name.last}</h4>
+          <p>${user.cell}</p>
+        </div>
+      </div>
+    `;
+
+    card.appendChild(cardHeader);
+    card.appendChild(cardContent);
+    content.appendChild(card);
+
+    body.classList.remove("animated-bg");
+  });
+};
+
+setTimeout(displayUser, 1000);
